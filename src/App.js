cb889d726795ab0d5920c9dbdbee203822607748
Bernardo Anderson <bernardodsanderson@gmail.com>
2017-03-06 11:38:19 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class App extends Component {
+
+  componentDidMount() {
+    Axios.get('https://www.justwatch.com/ca/provider/mubi')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
